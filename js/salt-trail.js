1feaba4bdc38e0a3142c4610c075270cbc914662
lowell1 <lowelljacobs@mail.com>
2019-08-27 18:07:27 -0700

    small mouse trail fix

--- js/salt-trail.js
+++ js/salt-trail.js
@@ -3,17 +3,24 @@ console.clear();
 let mouse_event_counter = 0;
 let current_trail_images_index = 0;
 let trail_images = document.getElementsByClassName("salt-trail");
+// let mouse_has_moved = false;
 
 trail_images[0].style.visibility = "visible";
 
 document.addEventListener("mousemove", function(event) {
-    if(mouse_event_counter == 5) {
+    if(mouse_event_counter == 25) {
+        // if(!mouse_has_moved)
+        //     for(let i = 0; i < trail_images.length; i++) {
+        //         trail_images[i].style.visibility = "visible";
+        //         mouse_has_moved = true;
+        //     }
+
         mouse_event_counter = 1;
 
         trail_images[current_trail_images_index].style.left = event.clientX - 5 + "px";
         trail_images[current_trail_images_index].style.top = event.clientY - 5 + "px";
 
-        trail_images[current_trail_images_index].style.visibility = "visible";
+        // trail_images[current_trail_images_index].style.visibility = "visible";
 
         if(current_trail_images_index == (trail_images.length - 1))
             current_trail_images_index = 0;
@@ -22,7 +29,6 @@ document.addEventListener("mousemove", function(event) {
     } else
         mouse_event_counter++;
 });
-document.getElementById("trail1").style.visibility = "hidden";
 
 // function CreateTrail() {
 //     var count = 0;
